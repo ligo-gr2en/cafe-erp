7e9897a2e354e5d04a48014067b30eace4a80d8c
Almaz Kaliev <almaz.dulatovich@gmail.com>
2020-07-17 17:24:32 +0600

    add active link style

--- src/components/Navbar/NavbarLink.jsx
+++ src/components/Navbar/NavbarLink.jsx
@@ -2,6 +2,7 @@ import { NavLink } from 'react-router-dom';
 import styled from 'styled-components';
 
 export default styled(NavLink)`
+  position: relative;
   display: flex;
   align-items: center;
   justify-content: center;
@@ -9,6 +10,7 @@ export default styled(NavLink)`
   height: 48px;
   color: #dadada;
   text-decoration: none;
+  background-color: transparent;
   border-radius: 50%;
   transition: .3s all cubic-bezier(.4, , .2, 1);
 
@@ -20,6 +22,23 @@ export default styled(NavLink)`
 
   &.${props => props.activeClassName} {
     color: #2a2d34;
-    background-color: #fff;
+
+    &::before {
+      position: absolute;
+      top: -24px;
+      right: -19px;
+      z-index: 1;
+      width: 77px;
+      height: 96px;
+      content: "";
+
+      background-image: url("assets/images/active.svg");
+      background-repeat: no-repeat;
+      background-position: center;
+    }
+
+    & > i {
+      z-index: 2;
+    }
   }
 `;
