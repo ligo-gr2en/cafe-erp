75b0204e8f2611bdc34a0385bb2a9cfe44223ce2
Almaz Kaliev <almaz.dulatovich@gmail.com>
2020-08-13 18:52:44 +0600

    add navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, NavLink } from 'react-router-dom';
 import { ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import HomeRoundedIcon from '@material-ui/icons/HomeRounded';
+import CategoryRoundedIcon from '@material-ui/icons/CategoryRounded';
+import StraightenRoundedIcon from '@material-ui/icons/StraightenRounded';
+import AccountBalanceWalletRoundedIcon from '@material-ui/icons/AccountBalanceWalletRounded';
+import AttachMoneyRoundedIcon from '@material-ui/icons/AttachMoneyRounded';
+import { ButtonBase } from '@material-ui/core';
 import { Home, Products, Units, Wallets, Transactions } from './pages';
 
 const theme = createMuiTheme({
@@ -13,13 +19,65 @@ const App = () => {
   return (
     <ThemeProvider theme={theme}>
       <Router>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/products" component={Products} />
-          <Route path="/units" component={Units} />
-          <Route path="/wallets" component={Wallets} />
-          <Route path="/transactions" component={Transactions} />
-        </Switch>
+        <nav className="Navbar">
+          <ul className="Nav">
+            <li className="Nav__Item">
+              <ButtonBase
+                exact
+                to="/"
+                className="Nav__Link"
+                activeClassName="Nav__Link_active"
+                component={NavLink}>
+                <HomeRoundedIcon />
+              </ButtonBase>
+            </li>
+            <li className="Nav__Item">
+              <ButtonBase
+                to="/products"
+                className="Nav__Link"
+                activeClassName="Nav__Link_active"
+                component={NavLink}>
+                <CategoryRoundedIcon />
+              </ButtonBase>
+            </li>
+            <li className="Nav__Item">
+              <ButtonBase
+                to="/units"
+                className="Nav__Link"
+                activeClassName="Nav__Link_active"
+                component={NavLink}>
+                <StraightenRoundedIcon />
+              </ButtonBase>
+            </li>
+            <li className="Nav__Item">
+              <ButtonBase
+                to="/wallets"
+                className="Nav__Link"
+                activeClassName="Nav__Link_active"
+                component={NavLink}>
+                <AccountBalanceWalletRoundedIcon />
+              </ButtonBase>
+            </li>
+            <li className="Nav__Item">
+              <ButtonBase
+                to="/transactions"
+                className="Nav__Link"
+                activeClassName="Nav__Link_active"
+                component={NavLink}>
+                <AttachMoneyRoundedIcon />
+              </ButtonBase>
+            </li>
+          </ul>
+        </nav>
+        <main className="Content">
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/products" component={Products} />
+            <Route path="/units" component={Units} />
+            <Route path="/wallets" component={Wallets} />
+            <Route path="/transactions" component={Transactions} />
+          </Switch>
+        </main>
       </Router>
     </ThemeProvider>
   );
