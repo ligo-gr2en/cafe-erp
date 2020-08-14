dcfc2494de9080cf34327e044353e8e51dc21d06
Almaz Kaliev <almaz.dulatovich@gmail.com>
2020-08-14 20:26:56 +0600

    add elevation for card

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import classNames from 'classnames';
 
-const Card = ({ className, rounded, children }) => {
+const Card = ({ className, elevation, rounded, children }) => {
   return (
     <div className={classNames(
       'Card',
-      { 'Card_rounded': rounded },
+      {
+        'Card_rounded': rounded,
+        [`Card_elevation${elevation}`]: elevation,
+      },
       className)}>
       {children}
     </div>
